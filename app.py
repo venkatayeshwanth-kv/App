0b9f2c5baaa3205a5ad9b56cd04c566108382d97
Kanneti Venkata Yeshwanth Reddy <154482333+venkatayeshwanth-kv@users.noreply.github.com>
2026-08-31 22:24:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,17 +10,60 @@ imputer = joblib.load("imputer.pkl")
 feature_names = joblib.load("feature_names.pkl")
 zero_cols = joblib.load("zero_cols.pkl")
 
-pregnancies = st.number_input("Pregnancies", 0, 20, 1)
-glucose = st.number_input("Glucose", 0, 300, 120)
-bp = st.number_input("Blood Pressure", 0, 200, 70)
-skin = st.number_input("Skin Thickness", 0, 100, 20)
-insulin = st.number_input("Insulin", 0, 900, 80)
-bmi = st.number_input("BMI", 0.0, 70.0, 25.0)
-dpf = st.number_input("Diabetes Pedigree Function", 0.0, 3.0, 0.5)
-age = st.number_input("Age", 1, 120, 30)
+st.title("Diabetes Prediction")
+
+st.write("Enter patient details")
+
+pregnancies = st.number_input(
+    "Pregnancies",
+    min_value=0,
+    value=1
+)
+
+glucose = st.number_input(
+    "Glucose",
+    min_value=0.0,
+    value=120.0
+)
+
+blood_pressure = st.number_input(
+    "Blood Pressure",
+    min_value=0.0,
+    value=70.0
+)
+
+skin_thickness = st.number_input(
+    "Skin Thickness",
+    min_value=0.0,
+    value=20.0
+)
+
+insulin = st.number_input(
+    "Insulin",
+    min_value=0.0,
+    value=80.0
+)
+
+bmi = st.number_input(
+    "BMI",
+    min_value=0.0,
+    value=30.0
+)
+
+diabetes_pedigree = st.number_input(
+    "Diabetes Pedigree Function",
+    min_value=0.0,
+    value=0.47
+)
+
+age = st.number_input(
+    "Age",
+    min_value=1,
+    value=30
+)
 
 if st.button("Predict"):
-    input_df = pd.DataFrame([[pregnancies, glucose, bp, skin, insulin, bmi, dpf, age]],
+    input_df = pd.DataFrame([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree, age]],
                              columns=feature_names)
     input_df[zero_cols] = input_df[zero_cols].replace(0, np.nan)
     input_df[zero_cols] = imputer.transform(input_df[zero_cols])
